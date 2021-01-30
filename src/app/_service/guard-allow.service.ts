b1d791349dc7c4a4348d9c4c77aa4661f2a102cb
Freddy Mendoza <frmendozat@uni.pe>
2021-01-30 12:28:30 -0500

    added perfil component

--- src/app/_service/guard-allow.service.ts
+++ src/app/_service/guard-allow.service.ts
@@ -1,20 +1,18 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { LoginService } from './login.service';
-import { MenuService } from './menu.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from 'src/environments/environment';
-import { map } from 'rxjs/operators';
-import { Menu } from '../_model/menu';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class GuardService implements CanActivate {
+export class GuardAllowService implements CanActivate{
+
 
   constructor(
     private loginService: LoginService,
-    private menuService: MenuService,
     private router: Router
   ) { }
 
@@ -29,35 +27,13 @@ export class GuardService implements CanActivate {
       const helper = new JwtHelperService();
       let token = sessionStorage.getItem(environment.TOKEN_NAME);
       if (!helper.isTokenExpired(token)) {
-        //3) VERIFICAR SI TIENES EL ROL NECESARIO PARA ACCEDER A ESA PAGINA  
-        //url -> /consulta
-        let url = state.url;
-
-        const decodedToken = helper.decodeToken(token);
-        return this.menuService.listarPorUsuario(decodedToken.user_name).pipe(map((data: Menu[]) => {
-          this.menuService.setMenuCambio(data);
-
-          let cont = 0;
-          for (let m of data) {
-            if (url.startsWith(m.url)) {
-              cont++;
-              break;
-            }
-          }
-
-          if (cont > 0) {
-            return true;
-          } else {
-            this.router.navigate(['not-403']);
-            return false;
-          }
-        }));
+        return true;
       } else {
-        console.log("token expired")
         this.loginService.cerrarSesion();
         return false;
       }
     }
 
   }
+
 }
